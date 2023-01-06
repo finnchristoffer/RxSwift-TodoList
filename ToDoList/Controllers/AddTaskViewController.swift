dfd36f0645a9afe9fd6cfac6c2f29002efabb6db
finnchristoffer <finn.christoffer@gmail.com>
2023-01-06 12:31:01 +0700

    Designing the Add New Task Screen

---
+++ ToDoList/Controllers/AddTaskViewController.swift
@@ -0,0 +1,13 @@
+//
+//  AddTaskViewController.swift
+//  ToDoList
+//
+//  Created by Finn Christoffer Kurniawan on 06/01/23.
+//
+
+import Foundation
+import UIKit
+
+class AddTaskViewController: UIViewController {
+    
+}
